ef2d4b1e97bd87bbc9f5182f2d124b26a781c66f
Vikram Naik <26vikramnaik@gmail.com>
2023-11-22 12:04:01 +0530

    Authorization bearer headers

--- controllers/main.js
+++ controllers/main.js
@@ -3,38 +3,47 @@
 // send back to frontend
 
 const jwt = require('jsonwebtoken')
-const CustomAPIError = require("../errors/custom-error");
+const CustomAPIError = require('../errors/custom-error')
 
 // set up authentication so only the request with JWT can access the dashboard
 
 const login = async (req, res) => {
-  const { username, password } = req.body;
-  console.log(username, password);
+  const { username, password } = req.body
+  console.log(username, password)
 
   // mongo
   // Joi
   // check in controler
 
   if (!username || !password) {
-    throw new CustomAPIError("please provide a username and password", 400);
+    throw new CustomAPIError('please provide a username and password', 400)
   }
+  // just for demo in production use long, complex and unguesable string values!!!!!!!!!
+  // try to keep payload small, better experience for users
+  const id = new Date().getDate()
 
-  const token = jwt.sign({
-    
-  })
+  const token = jwt.sign(
+    {
+      id,
+      username,
+    }, // payload
+    process.env.JWT_SECRET,
+    { expiresIn: '30d' }
+  )
 
-  res.send("Fake login/Register/Signup");
-};
+  // res.send("Fake login/Register/Signup");
+  res.status(200).json({ msg: 'user created', token })
+}
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
+  const luckyNumber = Math.floor(Math.random() * 100)
   res.status(200).json({
-    msg: "Hello Vikram",
+    msg: 'Hello Vikram',
     secret: `Here is your authorized data, your luckey number is ${luckyNumber}`,
-  });
-};
+  })
+}
 
 module.exports = {
   login,
   dashboard,
-};
+}
